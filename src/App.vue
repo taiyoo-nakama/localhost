3776fd922ba5e3f309c4252e2f3458f7edd01566
仲間太陽 <taiyoonakama@gmail.com>
2021-05-19 13:39:29 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <button @click="increase">増加</button>
+    <p>{{ $store.state.count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    increase(){
+      this.$store.dispatch("incrementAction");
+    }
+  }
+};
+</script>
+
